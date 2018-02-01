20b2426f67084379f1b28bc6d1c44aa1954c0fc8
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-01 06:53:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
 
 end
 
@@ -126,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
 
 end
